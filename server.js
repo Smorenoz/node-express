92a9b0223e33e69eb5451a0b79cc2fc5f278de1e
Salomon Moreno <84518068+Smorenoz@users.noreply.github.com>
2022-04-12 11:29:04 -0500

    awesome new commit!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('RRV Teams welcome you to this adventure!');
 });
 
 app.get('/health', (req, res) => {
